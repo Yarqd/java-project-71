53531c452ea6fe002ab73ee001a37d756b33bf70
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2024-04-17 14:40:15 +0300

    Update projest

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -8,38 +8,38 @@ import hexlet.code.formatters.StylishFormatter;
 import hexlet.code.formatters.PlainFormatter;
 import org.junit.jupiter.api.Test;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 class AppTest {
 
     private final String basePath = Paths.get("src", "test", "resources").toString();
+    private String readFixture(String path) throws IOException {
+        Path filePath = Paths.get(basePath, path);
+        return Files.readString(filePath);
+    }
+
+
 
     @Test
     void testJsonDiffWithStylishFormatter() throws Exception {
         File firstFile = new File(basePath, "file1.json");
         File secondFile = new File(basePath, "file2.json");
         Formatter formatter = new StylishFormatter();
-        String expected = "{\n"
-                + "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n"
-                + "}";
+        String expected = readFixture("fixtures/fixture1.json");
         String actual = Differ.generate(firstFile, secondFile, formatter);
-        assertEquals(expected, actual);
+        assertEquals(expected.trim(), actual.trim());
     }
+
 
     @Test
     void testYamlDiffWithPlainFormatter() throws Exception {
         File firstFile = new File(basePath, "file1.yml");
         File secondFile = new File(basePath, "file2.yml");
         Formatter formatter = new PlainFormatter();
-        String expected = "Property 'follow' was removed\n"
-                + "Property 'proxy' was removed\n"
-                + "Property 'timeout' was updated. From 50 to 20\n"
-                + "Property 'verbose' was added with value: true\n";
+        String expected = readFixture("fixtures/fixture3.json");
         String actual = Differ.generate(firstFile, secondFile, formatter);
         assertEquals(expected.trim(), actual.trim());
     }
